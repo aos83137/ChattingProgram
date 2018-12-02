c2985720532c5bd871932fff0a30974dd455ff26
A-3 <39228935+aos83137@users.noreply.github.com>
2018-12-02 15:44:41 +0900

    Update GameJava12.java

--- Chatting/java12/GameJava12.java
+++ Chatting/java12/GameJava12.java
@@ -1,126 +1,99 @@
-package java12;
+import java.net.*;
+import java.io.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import java.applet.Applet;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextArea;
-import java.awt.TextField;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.StringTokenizer;
-
-public class GameJava12 extends Applet 
-implements ActionListener, Runnable
+public class GameJava2_12 extends Applet
+  implements ActionListener, Runnable
 {
-	Socket mySocket =null;
-	PrintWriter out =null;
-	BufferedReader in =null;
-	
-	Thread clock;
-	TextArea memo;
-	Label label;
-	TextField list;
-	TextField name;
-	TextField input;
-	Panel NorthPanel;
-	Panel myPanel;
-	
-	
-	public void init() {
-		try {
-			setSize(600,300);
-			mySocket = new Socket("192.168.219.200",2486);
-			out = new PrintWriter(new OutputStreamWriter(mySocket.getOutputStream(),"KSC5601"), true);
-			in = new BufferedReader(new InputStreamReader(mySocket.getInputStream(),"KSC5601"),1024);
-		}catch(UnknownHostException e) {
-			System.out.println(e.toString());
-		}catch(IOException e) {
-			System.out.println(e.toString());
-		}
-		
-		//GUI
-		setLayout(new BorderLayout());
-		label = new Label("¡¢º” ∏Ì¥‹");
-		label.setBackground(Color.GRAY);
-		list = new TextField(60);
-		NorthPanel = new Panel();
-		NorthPanel.add(label);
-		NorthPanel.add(list);
-		add("North",NorthPanel);
-		memo = new TextArea(10,55);
-		add("Center",memo);
-		myPanel =  new Panel();
-//		name =new TextField(8);
-//		name.setText("¥Î»≠∏Ì");
-//		myPanel.add(name);
-		input = new TextField(40);
-		input.addActionListener(this);
-		myPanel.add(input);
-		add("South", myPanel);
-		}
-	
-	public void start() {
-		if(clock==null) {
-			clock=new Thread(this);
-			clock.start();
-		}
-	}
-	public void run() {
-		out.println("LOGIN|" +mySocket);
-		memo.append("¡¢º” " + getCodeBase().toString() + "\n");
-		
-		try {
-			while(true) {
-				String msg = in.readLine();
+  Socket mySocket= null;
+  PrintWriter out= null;
+  BufferedReader in= null;
 
-				StringTokenizer st =null;//√ﬂ∞°	
-				if(!msg.equals("") && !msg.equals(null)) {
-					st = new StringTokenizer(msg, "|");
-					String command = st.nextToken();
-					String text = st.nextToken();
-					if(command.equals("TALK")) {
-						memo.append(text + "\n");
-					}else if(command.equals("LIST")) {
-						list.setText(text);
-					}
-				}
-			}
-		}catch(IOException e) {
-			memo.append(e.toString());
-		}
-	}
-	
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == input) {
-			String data = input.getText();
-			input.setText("");
-						
-			out.println("TALK|"+data); //√ﬂ∞°
-			out.flush();
-		}
-	}
-	public void destroy() {
-		if(clock !=null &&clock.isAlive()) {
-			clock=null;
-		}
-		out.println("LOGOUT|" + name.getText());
-		out.flush();
-		
-		try {
-			out.close();
-			in.close();
-			mySocket.close();
-		}catch(IOException e) {
-			memo.append(e.toString());
-		}
-	}
+  Thread clock;
+  TextArea memo;
+  TextField name;
+  TextField input;
+  Panel myPanel;
+  
+  public void init()
+  {
+    try{
+      mySocket= new Socket(getCodeBase().getHost(),2587);
+      out= new PrintWriter(new OutputStreamWriter(mySocket.getOutputStream(), "KSC5601"), true);
+      in= new BufferedReader(new InputStreamReader(mySocket.getInputStream(), "KSC5601"), 1024);
+    }catch(UnknownHostException e){
+      System.out.println(e.toString());
+    }catch(IOException e){
+      System.out.println(e.toString());
+    }
+
+    // GUI
+    setLayout(new BorderLayout());
+    memo= new TextArea(10, 55);
+    add("Center", memo);
+
+    myPanel= new Panel();
+    name= new TextField(8);
+    name.setText("ÎåÄÌôîÎ™Ö");
+    myPanel.add(name);
+    input= new TextField(40);
+    input.addActionListener(this);
+    myPanel.add(input);
+    add("South", myPanel);
+  }
+
+  public void start()
+  {
+    if(clock==null){
+      clock= new Thread(this);
+      clock.start();
+    }
+  }
+
+  public void run()
+  {
+    out.println("LOGIN|"+ mySocket);
+    memo.append("[Ï†ëÏÜç] "+ getCodeBase().toString() +"\n");
+
+    try{
+      while(true){
+        String msg= in.readLine();
+        if(!msg.equals("") && !msg.equals(null)){
+          memo.append(msg +"\n");
+        }
+      }
+    }catch(IOException e){
+      memo.append(e.toString() +"\n");
+    }
+  }
+
+  public void actionPerformed(ActionEvent e)
+  {
+    if(e.getSource()==input){
+      String data= input.getText();
+      input.setText("");
+      out.println("TALK|"+ name.getText() +": "+ data);
+      out.flush();
+    }
+  }
+
+  public void stop()
+  {
+    if((clock!=null)&&(clock.isAlive())){
+      clock= null;
+    }
+
+    out.println("LOGOUT|"+ name.getText());
+    out.flush();
+
+    try{
+      out.close();
+      in.close();
+      mySocket.close();
+    }catch(IOException e){
+      memo.append(e.toString() +"\n");
+    }
+  }
 }
